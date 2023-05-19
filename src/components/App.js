067771bf8388c0d9f70541144d80d05222a9e8f0
pravin bhagwat <pravinbhagwat150@gmail.com>
2023-05-19 16:06:54 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const App = () => {
+function App(){
     return (
         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     );
