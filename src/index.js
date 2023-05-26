adeedf1336bc52ed6a3ba78d814bcbea92971bc3
pravin bhagwat <pravinbhagwat150@gmail.com>
2023-05-26 08:27:08 +0530

    DOne

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
 
 
