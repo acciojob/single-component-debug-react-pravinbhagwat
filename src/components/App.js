2dbb4ae31f565330af6a928df49c6149d091399f
pravin bhagwat <pravinbhagwat150@gmail.com>
2023-05-26 08:29:28 +0530

    DOne

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function App(){
     return (
-        <p id = "root">Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        <p id = "main">Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     );
 }
 
